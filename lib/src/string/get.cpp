887655de0ddd831e36475ddd12279c5e8ae15791
Данил Лутцев <kepleomax@MacBook-Air-Danil.local>
2026-05-14 22:01:30 +0300

    Made GET

--- lib/src/string/get.cpp
+++ lib/src/string/get.cpp
@@ -1,15 +1,25 @@
 #include "DataBase.h"
 #include <vector>
-
+#include <string>
+#include <expected>
 
 CommandResult DataBase::Get(const std::vector<std::string>& args) {
+    if (args.size() != 2) {
+        return std::unexpected("(error) wrong number of arguments for 'SET'");
+    }
+    
+    const std::string& key = args[1];
+
+    auto it = db_.find(key);
+    if (it == db_.end()) {
+        return std::unexpected("(nil)");
+    }
+    if (it->second.type != ValueType::kString) {
+        return std::unexpected("(error) WRONGTYPE Operation against a key holding the wrong kind of value");
+    }
+
+    return std::get<std::string>(it->second.data);
+
 
 }
 
-// unknown command	(error) unknown command
-// wrong arg count	(error) wrong number of arguments for 'CMD'
-// wrong type	(error) WRONGTYPE Operation against a key holding the wrong kind of value
-// OOM	(error) OOM command not allowed when used memory > 'maxmemory'
-// GET нет ключа	(nil)
-// EXISTS нет ключа	0
-// TTL нет ключа	-2
---
+++ lib/src/string/set.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <optional>
+#include "DataBase.h"
+
+CommandResult DataBase::Set(const std::vector<std::string>& args) {
+    if (args.size() != 3) {
+        return std::unexpected("(error) wrong number of arguments for 'SET'");
+    }
+
+    const std::string& key = args[1];
+    const std::string& value = args[2];
+    
+    size_t new_weight = value.size() + key.size();
+    size_t old_weight = 0;
+
+    auto it = db_.find(key);
+    if (it != db_.end()) {
+        old_weight = it->second.memory_usage;
+    }
+
+    if (memory_limit_.has_value() && 
+        memory_usage_ - old_weight + new_weight > memory_limit_.value()) {
+        return std::unexpected("(error) OOM command not allowed when used memory > 'maxmemory'");
+    }
+
+    memory_usage_ = memory_usage_ - old_weight + new_weight;
+    Value val {new_weight, value, ValueType::kString, std::nullopt};
+    db_[key] = std::move(val);
+
+    return "OK";
+}
